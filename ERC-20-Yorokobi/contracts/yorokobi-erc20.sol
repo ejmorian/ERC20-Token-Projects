e5db3eae1e0f704fc696b711aa09d61d5792758c
ejmorian <ejmorian@gmail.com>
2023-04-19 18:43:37 +0800

    add transfer function, getters

--- ERC-20-Yorokobi/contracts/yorokobi-erc20.sol
+++ ERC-20-Yorokobi/contracts/yorokobi-erc20.sol
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+error Yorokobi__TransactionFailed();
+error Yorokobi__InsufficientFunds();
+
 contract Yorokobi {
+    string private constant s_name = "Yorokobi";
+    string private constant s_symbol = "YRKB";
+    uint8 private constant s_decimals = 0;
+    uint256 private constant s_totalSupply = 1e15;
+    mapping(address => uint) private balances;
+
     constructor() {}
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
@@ -11,20 +20,42 @@ contract Yorokobi {
         uint256 _value
     );
 
-    function name() public view returns (string memory) {}
+    function name() public pure returns (string memory) {
+        return s_name;
+    }
 
-    function symbol() public view returns (string memory) {}
+    function symbol() public pure returns (string memory) {
+        return s_symbol;
+    }
 
-    function decimals() public view returns (uint8) {}
+    function decimals() public pure returns (uint8) {
+        return s_decimals;
+    }
 
-    function totalSupply() public view returns (uint256) {}
+    function totalSupply() public pure returns (uint256) {
+        return s_totalSupply;
+    }
 
-    function balanceOf(address _owner) public view returns (uint256 balance) {}
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
 
     function transfer(
         address _to,
         uint256 _value
-    ) public returns (bool success) {}
+    ) public returns (bool success) {
+        address _from = msg.sender;
+
+        if (balanceOf(msg.sender) > _value) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+
+            emit Transfer(_from, _to, _value);
+            return (success);
+        } else {
+            revert Yorokobi__InsufficientFunds();
+        }
+    }
 
     function transferFrom(
         address _from,
